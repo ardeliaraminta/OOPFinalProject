67dd707fbb5ef85036310a3f5d76734c49237e64
ardeliaraminta <ardelia.araminta@binus.ac.id>
2021-05-21 22:37:28 +0700

    doctor schedule

--- src/oopfinal/scheduleTime.java
+++ src/oopfinal/scheduleTime.java
@@ -1,5 +1,22 @@
 package oopfinal;
 
+import java.util.List;
+
 public class scheduleTime {
+    private int availTime;
+    List<Doctors> doctors;
+
+    public scheduleTime(int time, List<Doctors> doc) {
+        availTime = time;
+        doctors = doc;
+    }
+
+    public int getScheduleTime(){
+        return availTime;
+    }
+
+    public List<Doctors> getDoctors(){
+        return doctors;
+    }
 
 }
