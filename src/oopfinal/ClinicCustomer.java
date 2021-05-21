c012830a64d2103c987e99af3388c1cc3d0a157f
ardeliaraminta <ardelia.araminta@binus.ac.id>
2021-05-22 01:54:49 +0700

    Create ClinicCustomer.java

---
+++ src/oopfinal/ClinicCustomer.java
@@ -0,0 +1,81 @@
+package oopfinal;
+
+class ClinicCustomer {
+    private String userName,password, fullName, email, homeAddress;
+    private int phoneNumber;
+    private AquaticPet pets;
+
+    public ClinicCustomer(){
+    };
+
+    //constructor
+    public ClinicCustomer(String usr, String pwd, String fName, String mail, int phone, String add, AquaticPet pet) {
+        userName = usr;
+        password = pwd;
+        fullName = fName;
+        email = mail;
+        phoneNumber = phone;
+        homeAddress = add;
+        pets = pet;
+
+    }
+    // assign values to details of Customer
+    public void setUserName(String usr){
+        userName = usr;
+    }
+
+    public void setPassword(String pwd){
+        password = pwd;
+    }
+
+    public void setFullName(String fName){
+        fullName = fName;
+    }
+
+    public void setEmail(String mail) {
+        email = mail;
+    }
+
+    public void setPhoneNumber(int phone){
+        phoneNumber = phone;
+    }
+
+    public void setHomeAddress(String add){
+        homeAddress = add;
+    }
+
+    public void setPets(AquaticPet pet) {
+        pets = pet;
+    }
+
+    // getters of details of the Customer
+
+    public String getUserName(){
+        return userName;
+    }
+
+    public String getPassword(){
+        return password;
+    }
+
+    public String getFullName(){
+        return fullName;
+    }
+
+    public String getEmail(){
+        return email;
+    }
+
+    public int getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public String getHomeAddress() {
+        return homeAddress;
+    }
+
+    public AquaticPet getPets(){
+        return pets;
+    }
+
+}
